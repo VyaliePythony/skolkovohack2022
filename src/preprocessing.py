fb7576faeb350c79e23302b3671d9633d3daf27d
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2022-09-23 20:35:37 +0300

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,8 +4,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as sts
 data_candidates = pd.read_csv('data_candidates.csv',sep=';')
-data_candidates.rename(columns={'7435': "CandidateId", 'Водитель-экспедитор': "Position", '2': "Sex",
-                                'Россия': "Citizenship" ,'21' : 'Age', '0': 'Salary', 'NULL': ' Langs',
-                                'NULL.1' : 'DriverLicense','NULL.2' : 'Subway' , 'NULL.3': 'Skills','Full' : 'Employment','Full.1' : 'Shedule',
-                               'Санкт-Петербург' : 'CandidateRegion','2014-01-15 00:00:00.0000000' : 'DateCreated','163' : 'JobId','1425' : 'CandidateStatusId','Отклонен' : 'Status'
-                                })
+data_candidates = pd.concat([pd.DataFrame([data_candidates.columns.values], columns=['CandidateId', 'Position', 'Sex', 'Citizenship', 'Age', 'Salary', 'Langs', 'DriverLicense', 'Subway', 
+                                                                                     'Skills', 'Employment', 'Shedule','CandidateRegion','DateCreated', 'JobId', 'CandidateStatusId', 'Status']), data_candidates], ignore_index=True)

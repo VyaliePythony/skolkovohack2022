e4b57edcd99f6c391f20fb6838baee511c5216be
Zer0Nu11 <levo950@gmail.com>
2022-09-24 21:35:24 +0300

    data clean

--- src/data.py
+++ src/data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import re
 from string import punctuation
+from status_decode import stat
 
 def set_path(path):
     global data_path, test_path
@@ -32,7 +33,16 @@ def driver_license_fix(text):
     text = ' '.join(set(list(re.sub(r'[А-я ]+', "", text))))
     return ('категории '+text) if text else text
 
+def merge_list(text_list):
+    return ' '.join(text_list).strip()
+
 def load_data():
+    global data_candidates_workplaces, test_candidates_workplaces, \
+        data_candidates_education, test_candidates_education, \
+        data_candidates, test_candidates, \
+        data_jobs, test_jobs, \
+        status
+
     jobs_labels = ['JobId','Status','Name','Region','Description']
     test_jobs_labels = ['JobId','Status','Name','Region','Description','nan1','nan2','nan3']
     candidates_labels = ['CandidateId', 'Position', 'Sex', 'Citizenship', 'Age', 'Salary',
@@ -85,16 +95,43 @@ def load_data():
     data_candidates[['DriverLicense']] = data_candidates[['DriverLicense']].applymap(driver_license_fix)
     test_candidates[['DriverLicense']] = test_candidates[['DriverLicense']].applymap(driver_license_fix)
 
-    
+    data_candidates_pure = data_candidates[['CandidateId','Position','Langs','DriverLicense','Skills','CandidateRegion']]
+    status = data_candidates[['CandidateId', 'JobId', 'Status']]
 
-    return {
-        'train':{
-            'job':
-                [data_jobs],
-            'candidate':
-                [data_candidates, data_candidates_education, data_candidates_workplaces] },
-        'test':{
-            'job':
-                [test_jobs],
-            'candidate':
-                [test_candidates, test_candidates_education, test_candidates_workplaces] } }
+    test_candidates_workplaces = test_candidates_workplaces.groupby(['CandidateId'])['Position'].apply(list).reset_index()
+    data_candidates_workplaces = data_candidates_workplaces.groupby(['CandidateId'])['Position'].apply(list).reset_index()
+    test_candidates_education = test_candidates_education.groupby(['CandidateId'])['Faculty'].apply(list).reset_index()
+    data_candidates_education = data_candidates_education.groupby(['CandidateId'])['Faculty'].apply(list).reset_index()
+
+    test_candidates_workplaces[['Position']] = test_candidates_workplaces[['Position']].applymap(merge_list)
+    data_candidates_workplaces[['Position']] = data_candidates_workplaces[['Position']].applymap(merge_list)
+    test_candidates_education[['Faculty']] = test_candidates_education[['Faculty']].applymap(merge_list)
+    data_candidates_education[['Faculty']] = data_candidates_education[['Faculty']].applymap(merge_list)
+
+    data_candidates = data_candidates_pure.drop_duplicates(subset=['CandidateId'])
+    
+    data_candidates_tmp = pd.merge(data_candidates_education, data_candidates_workplaces, left_on='CandidateId', right_on='CandidateId')
+    data_candidates = pd.merge(data_candidates, data_candidates_tmp, left_on='CandidateId', right_on='CandidateId')
+    test_candidates_tmp = pd.merge(test_candidates_education, test_candidates_workplaces, left_on='CandidateId', right_on='CandidateId')
+    test_candidates = pd.merge(test_candidates, test_candidates_tmp, left_on='CandidateId', right_on='CandidateId')
+
+    # set metric instead of status + normalize
+    status[['Status']] = status[['Status']].applymap(lambda x: stat[x]/10.0)
+
+def get_soup_job(jobs):
+    tmp = jobs.copy()
+    def soup(row):
+        res = ' '.join([row['Name'],row['Description']])
+        res = re.sub(r'[ ]{2,}',' ',res)
+        return res
+    tmp['Soup'] = tmp[['Name','Description']].apply(soup, axis=1)
+    return tmp[['JobId','Region','Soup']]
+
+def get_soup_candidate(candidates):
+    tmp = candidates.copy()
+    def soup(row):
+        res = ' '.join([row['Position_x'],row['Langs'],row['DriverLicense'],row['Skills'],row['Faculty'],row['Position_y']])
+        res = re.sub(r'[ ]{2,}',' ',res)
+        return res
+    tmp['Soup'] = tmp[['Position_x','Langs','DriverLicense','Skills','Faculty','Position_y']].apply(soup, axis=1)
+    return tmp[['CandidateId','CandidateRegion','Soup']]
--- src/model.py
+++ src/model.py
@@ -7,7 +7,5 @@ import data
 data_path = 'C:\\Users\\zer0nu11\\Documents\\grad\\skolkovohack2022\\data\\for_hack_2022\\'
 
 data.set_path(data_path)
+data.load_data()
 
-dataset = data.load_data()
-df = dataset['train']['job']
-print(df)

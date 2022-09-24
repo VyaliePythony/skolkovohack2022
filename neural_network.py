54ce97032df412160b11246cb00785bef4c87918
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2022-09-25 02:11:14 +0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,38 +1,45 @@
+import torch
+from sklearn.model_selection import train_test_split
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
+from torch.utils.data import DataLoader,TensorDataset
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(625, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 10)
-        self.fc4 = nn.Linear(10, 1)
-    def forward(self, x):
-      x = F.relu(self.fc1(x))
-      x = F.relu(self.fc2(x))
-      x = F.relu(self.fc3(x))
-      x = self.fc4(x)
-      return F.log_softmax(x)
-model = Net()
-optimizer = optim.SGD(net.parameters(), lr=lr=1e-3, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
-from torch.utils.data import DataLoader
+embedding = np.load('embeddings.npy')
+status = np.load('status.npy').reshape(40570,1)
+region = np.load('region.npy').reshape(40570,1)
+embedding = np.concatenate((embedding, region), axis=1)
+x = torch.from_numpy(embedding)
+y = torch.from_numpy(status)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,
+                                                    shuffle=True, random_state=42)
+model = nn.Sequential(
+  nn.Linear(625, 200),
+  nn.ReLU(),
+  nn.Linear(200, 200), 
+  nn.ReLU(),
+  nn.Linear(200, 10),
+  nn.ReLU(),
+  nn.Linear(10, 1),
+  nn.Sigmoid()
+)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.99))
+criterion = nn.MSELoss()
+from torch.utils.data import DataLoader,TensorDataset
 
-batch_size = 128
+batch_size = 300
 epochs = 50
 history = []
-train_loader = DataLoader(train, batch_size=batch_size, drop_last=True)
-test_loader = DataLoader(test, batch_size=batch_size, drop_last=True)
-for i in range(epochs):
-  for x_batch, y_batch in train_loader:
-    x_batch = x_batch.view(x_batch.shape[0], -1).to(device)
-    y_batch = y_batch.to(device)
-
-    logits = model(x_batch)
-
-    loss = criterion(logits, y_batch)
+train_dataset = TensorDataset(x_train, y_train)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
+test_dataset = TensorDataset(x_test, y_test)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+for i in range(1000):
+    for x_batch,y_batch in train_loader:
+    
+    logits = model(x_batch.float())
+    #y_batch = y_batch.type(torch.LongTensor)
+    loss = criterion(logits, y_batch.float())
     history.append(loss.item())
 
     optimizer.zero_grad()
@@ -41,25 +48,3 @@ for i in range(epochs):
     optimizer.step()
 
   print(f'{i+1},\t loss: {history[-1]}')
-plt.figure(figsize=(10, 7))
-
-plt.plot(history)
-
-plt.title('Loss by batch iterations')
-plt.ylabel('Entropy Loss')
-plt.xlabel('batches')
-
-plt.show()
-acc = 0
-batches = 0
-
-for x_batch, y_batch in test_loader:
-  batches += 1
-  x_batch = x_batch.view(x_batch.shape[0], -1).to(device)
-  y_batch = y_batch.to(device)
-
-  preds = torch.argmax(model(x_batch), dim=1)
-  acc += (preds==y_batch).cpu().numpy().mean()
-
-print(f'Test accuracy {acc / batches:.3}')
-from sklearn.metrics import accuracy_score

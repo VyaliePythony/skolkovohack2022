821490fd90bea8dbb72c78940fb317f76de9bdab
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2022-09-24 17:07:20 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -68,23 +68,23 @@ def load_data():
             'Skills','CandidateRegion']] = \
         test_candidates[['Position','Langs','DriverLicense', \
             'Skills','CandidateRegion']].applymap(preprocess_signs)
-
     for i in tqdm(range(len(data_candidates))):
         skills=['']
-        if data_candidates['Skills'][i] != 0 and data_candidates['Skills'][i] != "||":
+        if data_candidates['Skills'][i] != '' and data_candidates['Skills'][i] != "||":
+            skills=['']
             skills = data_candidates['Skills'][i].split("||")
-            a = ''.join(skills)
+        a = ''.join(skills)
         data_candidates['Skills'][i] = a
 
     for i in tqdm(range(len(data_candidates))):
         languages = ['']
-        if data_candidates['Langs'][i] != 0 and data_candidates['Langs'][i] != "||":
+        if data_candidates['Langs'][i] != '' and data_candidates['Langs'][i] != "||":
             a = data_candidates['Langs'][i].split("||")
             for j in range(len(a)-1):
                 ll = a[j].split(':')
                 languages.append(ll[0]) 
-                b = ''.join(languages)
-        data_candidates['Langs'][i] = b
+        b = ' '.join(languages)
+        data_candidates['Langs'][i] = b 
 
     return {
         'train':{

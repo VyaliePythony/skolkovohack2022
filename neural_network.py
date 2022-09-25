da50972db65defaf10f62a5c961821a7827edfe2
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2022-09-25 04:47:27 +0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -14,17 +14,20 @@ y = torch.from_numpy(status)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,
                                                     shuffle=True, random_state=42)
 model = nn.Sequential(
-  nn.Linear(625, 200),
-  nn.ReLU(),
+  nn.Linear(625, 100),
+  nn.ReLU(), 
   nn.Linear(200, 200), 
   nn.ReLU(),
-  nn.Linear(200, 10),
+  nn.Dropout(),
+  nn.Linear(200, 60),
+  nn.ReLU(),
+  nn.Linear(100, 10), 
   nn.ReLU(),
   nn.Linear(10, 1),
   nn.Sigmoid()
 )
 optimizer = torch.optim.SGD(model.parameters(),lr = 0.01)
-criterion = nn.SmoothL1Loss()
+criterion = nn.MSELoss()
 from torch.utils.data import DataLoader,TensorDataset
 
 batch_size = 300
@@ -35,7 +38,7 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
 test_dataset = TensorDataset(x_test, y_test)
 test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
-for i in range(1000):
+for i in range(100):
     for x_batch,y_batch in train_loader:
     
         logits = model(x_batch.float())
@@ -49,3 +52,25 @@ for i in range(1000):
         optimizer.step()
 
     print(f'{i+1},\t loss: {history[-1]}')
+
+plt.figure(figsize=(10, 7))
+
+plt.plot(history)
+
+plt.title('Loss ')
+plt.ylabel('MSELoss')
+plt.xlabel('batches')
+
+plt.show()
+from sklearn.metrics import accuracy_score
+acc = 0
+batches = 0
+
+for x_batch, y_batch in test_loader:
+  batches += 1
+  preds = model(x_batch.float())
+  preds = torch.round(preds)
+  y_batch = torch.round(y_batch)
+  acc += (preds==y_batch).numpy().mean()
+
+print(f'Test accuracy {acc / batches:.3}')

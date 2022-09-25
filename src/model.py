d3bce66766b752c31eabd1fbe6832c5b788fcd25
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2022-09-25 05:51:32 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,5 +1,12 @@
 import pandas as pd
-
+import torch
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader,TensorDataset
+from sklearn.metrics import accuracy_score
+import numpy as np
+import matplotlib.pyplot as plt
 # dataset markup module
 import data
 
@@ -8,6 +15,21 @@ data_path = 'C:\\Users\\zer0nu11\\Documents\\grad\\skolkovohack2022\\data\\for_h
 
 data.set_path(data_path)
 
+model = nn.Sequential(
+  nn.Linear(625, 200),
+  nn.ReLU(), 
+  nn.Linear(200, 200), 
+  nn.ReLU(),
+  nn.Dropout(),
+  nn.Linear(200, 60),
+  nn.ReLU(),
+  nn.Linear(60, 10), 
+  nn.ReLU(),
+  nn.Linear(10, 1),
+  nn.Sigmoid()
+)
+optimizer = torch.optim.SGD(model.parameters(),lr = 0.01)
+criterion = nn.MSELoss()
 # train on dataset
 def train():
     data.load_data()
@@ -17,16 +39,63 @@ def train():
     # candidates.to_csv(data.data_path+'candidates_embed.csv')
     train = data.get_train_data(jobs,candidates,data.status)
     train = data.make_train_array(train)
-
+    y = train[:,625].reshape(40570,1)
+    x = torch.from_numpy(x)
+    y = torch.from_numpy(y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,
+                                                        shuffle=True, random_state=42)
     # TRAIN MODEL
+    batch_size = 300
+    epochs = 50
+    history = []
+    train_dataset = TensorDataset(x_train, y_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
+    test_dataset = TensorDataset(x_test, y_test)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
+    for i in range(100):
+        for x_batch,y_batch in train_loader:
+
+            logits = model(x_batch.float())
+            #y_batch = y_batch.type(torch.LongTensor)
+            loss = criterion(logits, y_batch.float())
+            history.append(loss.item())
+
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+
+        print(f'{i+1},\t loss: {history[-1]}')
+
+    plt.figure(figsize=(10, 7))
+
+    plt.plot(history)
+
+    plt.title('Loss ')
+    plt.ylabel('MSELoss')
+    plt.xlabel('batches')
+
+    plt.show()
+    from sklearn.metrics import accuracy_score
+    acc = 0
+    batches = 0
+
+    for x_batch, y_batch in test_loader:
+        batches += 1
+        preds = model(x_batch.float())
+        preds = torch.round(preds)
+        y_batch = torch.round(y_batch)
+        acc += (preds==y_batch).numpy().mean()
+
+    print(f'Test accuracy {acc / batches:.3}')
 # test on dataset
 def predict_test():
     data.load_data(raw=True)
     identities,test = data.pair_to_vec(data.test_jobs,data.test_candidates,data.test_candidates_workplaces,data.test_candidates_education)
 
     # MODEL PREDICT ON TEST
-
+           
     # SET DEPENDENCIES OF PREDICTIONS BY IDENTITIES
 
 def predict(jobs,candidates,candidates_workplaces,candidates_education):
